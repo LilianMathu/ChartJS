2e4a0b12f86dc92ec4cd3c2861128fa3471705e1
lilian Mathu <lilianmathu2018@gmail.com>
2021-03-10 04:55:54 +0300

    set default props

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 import { Line, Bar, Pie } from 'react-chartjs-2';
 
 export default class Chart extends Component {
-    constructor(props){  //is a function that run when the component is initialized
+    constructor(props) {  //is a function that run when the component is initialized
         super(props);
         this.state = {
             chartData: {
                 labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
                 datasets: [
                     {
-                        label:'Population',
-                        data:[
+                        label: 'Population',
+                        data: [
                             617594,
                             181045,
                             153060,
                             106519,
                             105162,
                             95072
-                          ],
-                          backgroundColor:[
+                        ],
+                        backgroundColor: [
                             'rgba(255, 99, 132, 0.6)',
                             'rgba(54, 162, 235, 0.6)',
                             'rgba(255, 206, 86, 0.6)',
@@ -26,13 +26,23 @@ export default class Chart extends Component {
                             'rgba(153, 102, 255, 0.6)',
                             'rgba(255, 159, 64, 0.6)',
                             'rgba(255, 99, 132, 0.6)'
-                          ]
+                        ]
                     }
                 ]
             }
-        }
+        };
 
-    }
+
+
+    };
+
+    //set default props
+    static defaultProps = {
+        displayTitle: true,
+        displayLegend: true,
+        legendPosition: "right"
+    };
+
     render() {
         return (
             <div>
@@ -41,14 +51,14 @@ export default class Chart extends Component {
                         data={this.state.chartData}
                         options={{
                             title: {
-                                display:true,
+                                display: this.props.displayTitle,
                                 text: "Largest cities in Massachusetts",
-                                fontSize: 25 
+                                fontSize: 25
                             },
-                            
+
                             legend: {
-                                display: true,
-                                position: 'right'
+                                display: this.props.displayLegend,
+                                position: this.props.legendPosition
                             }
                         }}
                     />
